4b0766f58b060093b236a6051e6b0df34249b046
Jelmer Visser <jelmer@weflycheap.nl>
2019-11-09 11:55:53 +0100

    Use strapi upload plugin

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -28,7 +28,7 @@ import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
-import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
+import SimpleUploadAdapter from 'ckeditor5-upload-strapi/src/adapters/simpleuploadadapter';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
