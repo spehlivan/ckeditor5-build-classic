c2b66e0cd4a2b9ae8a4b21985efd6727125c063e
Jelmer Visser <jelmer@weflycheap.nl>
2019-11-10 15:42:16 +0100

    New Strapi upload provider

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -28,7 +28,7 @@ import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
-import SimpleUploadAdapter from 'ckeditor5-upload-strapi/src/adapters/simpleuploadadapter';
+import SimpleUploadAdapterStrapi from 'ckeditor5-upload-strapi/src/adapters/simpleuploadadapterstrapi';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -56,7 +56,7 @@ ClassicEditor.builtinPlugins = [
 	PasteFromOffice,
 	Table,
 	TableToolbar,
-	SimpleUploadAdapter
+	SimpleUploadAdapterStrapi
 ];
 
 // Editor configuration.
